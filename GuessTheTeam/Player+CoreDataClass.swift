ab33fd70d966afb2eea06df406d876de322f7ad5
Eduardo Regis <eduregis1@gmail.com>
2021-02-03 14:54:52 -0300

    creating, reading and deleting teams

---
+++ GuessTheTeam/Player+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Player+CoreDataClass.swift
+//  GuessTheTeam
+//
+//  Created by Eduardo Oliveira on 03/02/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Player)
+public class Player: NSManagedObject {
+
+}
--- GuessTheTeam/SceneDelegate.swift
+++ GuessTheTeam/SceneDelegate.swift
@@ -10,9 +10,7 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
     
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
@@ -25,7 +23,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
            window?.makeKeyAndVisible()
 
     }
-
     
     func sceneDidEnterBackground(_ scene: UIScene) {
         // Called as the scene transitions from the foreground to the background.
@@ -33,7 +30,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // to restore the scene back to its current state.
 
         // Save changes in the application's managed object context when the application transitions to the background.
-        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+        DatabaseController.saveContext()
     }
 
 
